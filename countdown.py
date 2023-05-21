525d2d1fb3f532eeee6292ec31207e224aa4f3f4
Miguel Matos <miguelamatos@av.it.pt>
2023-05-21 23:08:47 +0100

    from machine

--- countdown.py
+++ countdown.py
@@ -8,7 +8,7 @@ def main():
     while True:
         r = session.get("http://localhost:8000/listener/countdown")
         print(json.loads(r.text)["countdown"])
-        sleep(10)
+        sleep(5)
 
 if __name__ == "__main__":
     main()
