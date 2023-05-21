4ff9de863d7dacc2bd8c97971c141cef79644172
Miguel Matos <miguelamatos@ua.pt>
2023-05-21 22:53:22 +0100

    countdown :)

--- countdown.py
+++ countdown.py
@@ -1,7 +1,14 @@
-import requests
+import requests, random
+from time import sleep
+
+session = requests.Session()
+session.verify = False
 
 def main():
-    pass
+    while True:
+        sleep(random.randint(1, 10))
+        r = session.get("https://localhost:8000/listener/countdown")
+        print(r.text)
 
 if __name__ == "__main__":
     main()
